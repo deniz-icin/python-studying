414535fc18264539453be473fe720ad71e17487d
deniz icin <denizicinn10@gmail.com>
2022-09-27 11:52:31 +0300

    multiples URL supported

--- basics/01_strings/main.py
+++ basics/01_strings/main.py
@@ -1,14 +1,21 @@
-URL = "https://meet.google.com/yig-qocf-xmc?pli=1"
 
-nakedURL = URL
+URL_list = ["http://meet.google.com/yig-qocf-xmc?pli=1","https://meet.google.com/yig-qocf-xmc?pli=1","https://www.notion.so/balconyke/continue-7709144555a64d0191f270340bcb43840","www.codecademy.com/learn/paths/front-end-engineer-career-path"]
 
-if nakedURL.startswith("https://"):
-    nakedURL = nakedURL.replace("https://","")
+for URL in URL_list:
 
-if nakedURL.startswith("http://"):
-    nakedURL = nakedURL.replace("http://","")
 
-y = nakedURL.split("/")
 
-print(y[0])
+    # URL = "https://meet.google.com/yig-qocf-xmc?pli=1"
+
+    nakedURL = URL
+
+    if nakedURL.startswith("https://"):
+        nakedURL = nakedURL.replace("https://","")
+
+    if nakedURL.startswith("http://"):
+        nakedURL = nakedURL.replace("http://","")
+
+    y = nakedURL.split("/")
+
+    print(y[0])
 
