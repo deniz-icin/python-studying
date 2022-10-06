1bba6af37a79a4f5b364039fcf3dae7181992658
deniz icin <denizicinn10@gmail.com>
2022-10-06 19:39:23 +0300

    phone valid is not true

--- basics/01_strings/hackerrank/challenge8.py
+++ basics/01_strings/hackerrank/challenge8.py
@@ -5,11 +5,13 @@ phone_book = {}
 for _ in range(n):
     name,phone=input().split()
     phone_book[name] = [phone]
-    d = phone_book
-    
+
+d = phone_book    
+
+
 for _ in d:
     query = input()
     if _ != query:
         print("Not found")
     else:
-        print(name + "=" + phone)
+        print(query + "=" + phone)
