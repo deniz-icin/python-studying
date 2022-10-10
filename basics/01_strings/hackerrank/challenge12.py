241f48893a6efe4982cca16a04579ae580dae219
deniz icin <denizicinn10@gmail.com>
2022-10-10 18:49:52 +0300

    typerror for __init__ arguments

---
+++ basics/01_strings/hackerrank/challenge12.py
@@ -0,0 +1,37 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    def __init__(self, scores):
+        self.scores = scores
+    def calculate(self):
+        average_score = sum(scores)/len(scores)
+        if (90 <= average_score <= 100):
+            return 'O'
+        if (80 <= average_score < 90):
+            return 'E'
+        if (70 <= average_score < 80):
+            return 'A'
+        if (55 <= average_score < 70):
+            return 'P'
+        if (40 <= average_score < 55):
+            return 'D'
+        if (average_score <40):
+            return 'T'
+        
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
