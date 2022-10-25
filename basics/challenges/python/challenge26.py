66a563081e127bad254fe970865c30b34a398f11
hariboi35 <denizicinn10@gmail.com>
2022-10-25 18:52:35 +0300

    correct solution created

--- basics/challenges/python/challenge26.py
+++ basics/challenges/python/challenge26.py
@@ -1,10 +1,13 @@
-
 def main():
     fine = 0
     d1, m1, y1 = [int(d1) for d1 in input().split(" ")] #returned date
     d2, m2, y2 = [int(d2) for d2 in input().split(" ")] #due date
-    if y2 >= y1:
-        if m2 >= m1:
+    if y2 > y1:
+        print(fine)
+    elif y2 == y1:
+        if m2 > m1:
+            print(fine)
+        elif m2 == m1:
             if d2 >= d1:
                 print(fine)
             else:
